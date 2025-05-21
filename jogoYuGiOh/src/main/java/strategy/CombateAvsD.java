82c35a4a00bb5f68ebf088e089868f56ac8d6080
giovanna ramos <161260067+idlrwheel@users.noreply.github.com>
2025-05-21 12:47:28 -0300

    Update CombateAvsD.java

--- jogoYuGiOh/src/main/java/strategy/CombateAvsD.java
+++ jogoYuGiOh/src/main/java/strategy/CombateAvsD.java
@@ -8,6 +8,21 @@ package strategy;
  *
  * @author Giovanna
  */
-public class CombateAvsD {
-    
-}
+public class CombateAvsD implements EstrategiaCombate {
+@Override
+    public int executarCombate(Jogador jogador1, Carta cartaJogador1, String estadoJogador1, Jogador jogador2, Carta cartaJogador2, String estadoJogador2) {
+        System.out.println("Combate: Ataque vs Defesa");
+        System.out.println(jogador1.getNome() + " jogou: " +  " (Ataque: " + cartaJogador1.getAtaque() + ")");
+        System.out.println(jogador2.getNome() + " jogou: " + " (Defesa: " + cartaJogador2.getDefesa() + ")");
+
+        if (cartaJogador1.getAtaque() > cartaJogador2.getDefesa()) {
+            System.out.println(jogador1.getNome() + " vence a rodada!");
+            return 1;
+        } else if (cartaJogador2.getDefesa() > cartaJogador1.getAtaque()) {
+            System.out.println(jogador2.getNome() + " vence a rodada!");
+            return 2;
+        } else {
+            System.out.println("Empate na rodada!");
+            return 0;
+        }
+    }
