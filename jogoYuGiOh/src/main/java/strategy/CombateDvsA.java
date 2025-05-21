5115445b6b17a23dff737bdb872114a1d3b7e0c6
giovanna ramos <161260067+idlrwheel@users.noreply.github.com>
2025-05-21 19:00:59 -0300

    Update CombateDvsA.java

--- jogoYuGiOh/src/main/java/strategy/CombateDvsA.java
+++ jogoYuGiOh/src/main/java/strategy/CombateDvsA.java
@@ -15,14 +15,14 @@ public class CombateDvsA implements EstrategiaCombate {
 
     @Override
     public int executarCombate(Jogador jogador1, Carta cartaJogador1, String estadoJogador1, Jogador jogador2, Carta cartaJogador2, String estadoJogador2) {
-        System.out.println("Combate: Ataque vs Defesa");
-        System.out.println(jogador1.getNome() + " jogou: " +  " (Ataque: " + cartaJogador1.getAtaque() + ")");
-        System.out.println(jogador2.getNome() + " jogou: " + " (Defesa: " + cartaJogador2.getDefesa() + ")");
+        System.out.println("Combate: Defesa vs Ataque");
+        System.out.println(jogador1.getNome() + " jogou: " +  " (Defesa: " + cartaJogador1.getDefesa() + ")");
+        System.out.println(jogador2.getNome() + " jogou: " + " (Ataque: " + cartaJogador2.getDefesa() + ")");
 
-        if (cartaJogador1.getAtaque() > cartaJogador2.getDefesa()) {
+        if (cartaJogador1.getDefesa() > cartaJogador2.getAtaque()) {
             System.out.println(jogador1.getNome() + " vence a rodada!");
             return 1;
-        } else if (cartaJogador2.getDefesa() > cartaJogador1.getAtaque()) {
+        } else if (cartaJogador2.getAtaque() > cartaJogador1.getDefesa()) {
             System.out.println(jogador2.getNome() + " vence a rodada!");
             return 2;
         } else {
