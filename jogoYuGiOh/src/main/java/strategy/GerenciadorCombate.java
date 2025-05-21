baf5bd595c682d3d9aaedb87adc00113bc8f9c34
Vinicius Iago <160291470+VIONE34-PNG@users.noreply.github.com>
2025-05-21 18:04:39 -0300

    Update GerenciadorCombate.java

--- jogoYuGiOh/src/main/java/strategy/GerenciadorCombate.java
+++ jogoYuGiOh/src/main/java/strategy/GerenciadorCombate.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package strategy;
 
 import java.util.HashMap;
@@ -9,10 +5,6 @@ import java.util.Map;
 import model.Carta;
 import model.Jogador;
 
-/**
- *
- * @author Giovanna
- */
 public class GerenciadorCombate {
     private Map<String, EstrategiaCombate> estrategias;
 
@@ -20,26 +12,25 @@ public class GerenciadorCombate {
         this.estrategias = new HashMap<>();
         this.estrategias.put("ATAQUE vs ATAQUE", new CombateAvsA());
         this.estrategias.put("ATAQUE vs DEFESA", new CombateAvsD());
-        this.estrategias.put("DEFESA vs ATAQUE", new CombateDvsA()); 
+        this.estrategias.put("DEFESA vs ATAQUE", new CombateDvsA());
         this.estrategias.put("DEFESA vs DEFESA", new CombateDvsD());
     }
 
-    public int realizarCombate(Jogador jogador1, Carta cartaJogador1, String estadoJogador1,
+    public void realizarCombate(Jogador jogador1, Carta cartaJogador1, String estadoJogador1,
                                 Jogador jogador2, Carta cartaJogador2, String estadoJogador2) {
         String chaveCombate = estadoJogador1.toUpperCase() + " vs " + estadoJogador2.toUpperCase();
-
-        EstrategiaCombate estrategia;
-        if (chaveCombate.equals("DEFESA vs ATAQUE")) {
-            estrategia = estrategias.get("ATAQUE vs DEFESA");
-        } else {
-            estrategia = estrategias.get(chaveCombate);
-        }
+        EstrategiaCombate estrategia = estrategias.getOrDefault(chaveCombate, estrategias.get("ATAQUE vs DEFESA"));
 
         if (estrategia != null) {
-            return estrategia.executarCombate(jogador1, cartaJogador1, estadoJogador1, jogador2, cartaJogador2, estadoJogador2);
+            int resultado = estrategia.executarCombate(jogador1, cartaJogador1, estadoJogador1, jogador2, cartaJogador2, estadoJogador2);
+
+            if (resultado == 1) {
+                jogador1.adicionarPonto();
+            } else if (resultado == 2) {
+                jogador2.adicionarPonto();
+            }
         } else {
-            System.out.println("Estratégia de combate inválida para: " + chaveCombate);
-            return 0; 
+            System.out.println("Estratégia de combate inválida: " + chaveCombate);
         }
     }
 }
