ca8e7029fb4ae0412c72798db69f89e525395a39
Vitorwgs <167483937+Vitorwgs@users.noreply.github.com>
2025-05-21 11:25:25 -0300

    Update JogoYuGiOh.java

--- jogoYuGiOh/src/main/java/JogoYuGiOh.java
+++ jogoYuGiOh/src/main/java/JogoYuGiOh.java
@@ -1,16 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+package jogoYuGiOh;
 
+import model.Jogador;
+import strategy.GerenciadorCombate;
+import state.JogoContexto;
 
-
-/**
- *
- * @author Giovanna
- */
 public class JogoYuGiOh {
 
     public static void main(String[] args) {
-        
+        Jogador jogador1 = new Jogador("Yugi");
+        Jogador jogador2 = new Jogador("Kaiba");
+        GerenciadorCombate gerenciador = new GerenciadorCombate();
+        JogoContexto contexto = new JogoContexto();
+
+        contexto.executar(jogador1, jogador2, gerenciador); 
+        contexto.executar(jogador1, jogador2, gerenciador); 
+        contexto.executar(jogador1, jogador2, gerenciador); 
+        contexto.executar(jogador1, jogador2, gerenciador); 
     }
 }
